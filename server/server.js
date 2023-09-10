58b89b55bf1955c995ea042572ea91490fe5e7a5
King-fReeBoDy <89941338+King-fReeBoDy@users.noreply.github.com>
2023-09-10 18:14:13 +0000

    register user

--- server/server.js
+++ server/server.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
+const bcryt = require("bcrypt");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
 
 const app = express();
 const PORT = 8080;
@@ -9,7 +13,26 @@ const PORT = 8080;
 app.use(cors());
 app.use(cookieParser());
 
-app.post("/register", (req, res) => {});
+app.post("/register", async (req, res) => {
+  const { username, email, password } = req.body;
+
+  if (!username || !email || !password) {
+    return res.send("Enter all credentials");
+  }
+
+  try {
+    const salt = await bcryt.genSalt(10);
+    const hashPassword = await bcryt.hash(password, salt);
+    const user = await prisma.create({
+      data: { username, email, password: hashPassword },
+    });
+
+    res.status(201).json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal error");
+  }
+});
 app.post("/login", (req, res) => {});
 app.get("/logout", (req, res) => {});
 
